32d2fb2dd14d458af4072aab8dba89191ded4aee
Mohammad Owais <mohammadowais241198@gmail.com>
2021-07-25 00:58:15 +0530

    story done animation also

--- app/src/main/java/com/i/instaton/ui/story/StoryActivity.kt
+++ app/src/main/java/com/i/instaton/ui/story/StoryActivity.kt
@@ -3,7 +3,12 @@ package com.i.instaton.ui.story
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.ViewPropertyAnimator
+import android.widget.Toast
 import androidx.activity.viewModels
+import androidx.viewpager2.widget.ViewPager2
 import com.i.instaton.R
 import com.i.instaton.databinding.ActivityStoryBinding
 
@@ -12,6 +17,7 @@ class StoryActivity : AppCompatActivity() {
     private val storyViewModel by viewModels<StoryViewModel>()
     private lateinit var _binding: ActivityStoryBinding
     private val storyPagerAdapter= StoryPagerAdapter()
+    private val handler = Handler(Looper.getMainLooper())
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,7 +31,24 @@ class StoryActivity : AppCompatActivity() {
 
         _binding.storyViewPager.adapter = storyPagerAdapter
 
+        _binding.storyViewPager.registerOnPageChangeCallback(pageChangeCallback)
+    }
 
+    private val nextPageRunnable = Runnable {
+        _binding.storyViewPager.currentItem++
+    }
+
+    private val pageChangeCallback = object: ViewPager2.OnPageChangeCallback(){
+        override fun onPageSelected(position: Int) {
+            super.onPageSelected(position)
+
+            _binding.progressView.scaleX = 0F
+            _binding.progressView.animate().cancel()
+            _binding.progressView.animate().scaleX(1F).setDuration(5000).setStartDelay(10).start()
+
+            handler.removeCallbacks(nextPageRunnable)
+            handler.postDelayed(nextPageRunnable, 5000)
+        }
     }
 
     override fun onResume() {
